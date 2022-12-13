df951e131f9a43615470e0d5ea5ab262b2c075a4
adityaputtigampala <aditya.puttigampala@gmail.com>
2022-12-13 10:31:11 -0500

    update to likes and tuits

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,17 +1,23 @@
-import React from "react";
-import './tuits.css';
 import Tuit from "./tuit";
+import * as likesService from "./likes-service";
+const Tuits = ({tuits = [], deleteTuit,
+                refreshTuits}) => {
 
-function Tuits({tuits = [], deleteTuit}) {
-    return (
+  const likeTuit = (tuit) =>
+    likesService
+      .userTogglesTuitLikes("me", tuit._id)
+      .then(refreshTuits)
+      .catch(e => alert(e))
+
+  return (
     <div>
-      <ul className="ttr-tuits list-group">
+      <ul>
         {
-          tuits.map && tuits.map(tuit => {
-            return (
-              <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
-            );
-          })
+          tuits.map(tuit =>
+            <Tuit key={tuit._id}
+              deleteTuit={deleteTuit}
+              likeTuit={likeTuit}
+              tuit={tuit}/>)
         }
       </ul>
     </div>
--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,11 +1,6 @@
 import React from "react";
-
-export default class TuitStats extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
+const TuitStats = ({tuit, likeTuit}) => {
+  return (
       <div className="row mt-2">
         <div className="col">
           <i className="far fa-message me-1"></i>
@@ -16,13 +11,24 @@ export default class TuitStats extends React.Component {
           {this.props.tuit.stats && this.props.tuit.stats.retuits}
         </div>
         <div className="col">
-          <i className="far fa-heart me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.likes}
-        </div>
+                <span onClick={() => likeTuit(tuit)}>
+                {
+                    tuit.stats.likes > 0 &&
+                    <i className="fas fa-thumbs-up pe-2"
+                       style={{color: 'green'}}></i>
+                }
+                {
+                    tuit.stats.likes <= 0 &&
+                    <i className="far fa-thumbs-up pe-2"></i>
+                }
+                {tuit.stats && tuit.stats.likes}
+                </span>
+          </div>
         <div className="col">
           <i className="far fa-inbox-out"></i>
         </div>
       </div>
     );
   }
-}
+  export default TuitStats;
+
