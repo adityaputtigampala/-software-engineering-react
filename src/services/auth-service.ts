e78fdf30da881da8602d7df63781ba09e1443646
adityaputtigampala <aditya.puttigampala@gmail.com>
2022-12-12 22:38:01 -0500

    react changes

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -10,3 +10,15 @@ const api = axios.create({
 export const signup = (user) =>
    api.post(`${AUTH_API}/signup`, user)
        .then(response => response.data);
+
+export const login = (user) =>
+api.post(`${AUTH_API}/login`, user)
+   .then(response => response.data);
+
+export const profile = () =>
+      api.post(`${AUTH_API}/profile`)
+         .then(response => response.data);
+
+export const logout = (user) =>
+      api.post(`${AUTH_API}/logout`, user)
+         .then(response => response.data);
